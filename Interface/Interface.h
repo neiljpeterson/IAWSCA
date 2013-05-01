782ca0b85a37d809ea856b79a17f00c34d5a61b4
Neil Peterson <github@x5w.org>
2013-04-30 20:58:35 -0500

    fixed a bug with title in Interface::newMenu, playing with a documentation system

--- Interface/Interface.h
+++ Interface/Interface.h
@@ -21,9 +21,21 @@ class Interface{
 public:
 	Interface(){};
 	
-	void newMenu(string name,string title,vector<string> menu){
-		menu.push_back(title);
-		menus.insert(make_pair(name,menu));
+	/** \brief Creates and stores a menu for later use
+	 *  \param name A unique name given to the menu
+	 *  \param title The title that will appear at the top of the menu, this is the key value for accessing the menu in the future
+	 *  \param menu These strings will be the menu, in the order they are in the container
+	 *  \return Returns false if the menu is not created because the name was already taken
+	 */
+	bool newMenu(string name,string title,vector<string> menu){
+		menu.insert(menu.begin(),title);
+		if (menus.find(name) == menus.end()){
+			menus.insert(make_pair(name,menu));
+			return true;
+		}
+		//else
+		return false;
+		
 	}
 	
 	void showMenu(string name,bool numbers = true){
@@ -33,6 +45,9 @@ public:
 		
 	}
 	
+	/** \brief Returns the number of options in the menu
+	 *  \param name The name the menu was given during newMenu()
+	 */
 	int getMenuSize(string name){
 		return menus[name].size();
 	}
